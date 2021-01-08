0370b2f694124d7d978e4de4b53ede5c95ead134
Ayodele Oluwasina <ayodeleoluwasina@gmail.com>
2021-01-08 18:35:00 -0500

    n

--- js/landingpage.js
+++ js/landingpage.js
@@ -1,3 +1,26 @@
+//Paystack
+var paymentForm = document.getElementById('paymentForm');
+paymentForm.addEventListener('submit', payWithPaystack, false);
+function payWithPaystack() {
+  var handler = PaystackPop.setup({
+    key: 'pk_test_9245c8d4e7b803050a19fac3daffe93d2ecf8a92', // Replace with your public key
+    email: document.getElementById('email-address').value,
+    amount: 1000 * 100, // the amount value is multiplied by 100 to convert to the lowest currency unit
+    currency: 'NGN', // Use GHS for Ghana Cedis or USD for US Dollars
+    ref: 'aa', // Replace with a reference you generated
+    callback: function (response) {
+      //this happens after the payment is completed successfully
+      var reference = response.reference;
+      alert('Payment complete! Reference: ' + reference);
+      // Make an AJAX call to your server with the reference to verify the transaction
+    },
+    onClose: function () {
+      alert('Transaction was not completed, window closed.');
+    },
+  });
+  handler.openIframe();
+}
+
 // HEADER ANIMATION
 window.onscroll = function () {
   scrollFunction();
@@ -85,25 +108,3 @@ window.onresize = function () {
     $('#navbarSupportedContent').removeClass('show');
   }
 };
-
-//Paystack
-const paymentForm = document.getElementById('paymentForm');
-paymentForm.addEventListener('submit', payWithPaystack, false);
-function payWithPaystack(e) {
-  e.preventDefault();
-  let handler = PaystackPop.setup({
-    key: 'pk_test_9245c8d4e7b803050a19fac3daffe93d2ecf8a92', // Replace with your public key
-    email: document.getElementById('email-address').value,
-    amount: document.getElementById('amount').value * 100,
-    ref: '' + Math.floor(Math.random() * 1000000000 + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
-    // label: "Optional string that replaces customer email"
-    onClose: function () {
-      alert('Window closed.');
-    },
-    callback: function (response) {
-      let message = 'Payment complete! Reference: ' + response.reference;
-      alert(message);
-    },
-  });
-  handler.openIframe();
-}
